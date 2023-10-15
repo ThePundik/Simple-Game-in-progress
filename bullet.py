46a1a374ef5adfb8de51d0525d4866a6460e9bf4
ThePundik <thepundik@gmail.com>
2023-10-16 00:29:43 +0200

    Simple game on PyCharm

---
+++ bullet.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Bullet(pygame.sprite.Sprite):
+
+    def __init__(self, screen, gun):
+        """создаем пулю в позиции пушки"""
+        super(Bullet, self).__init__()
+        self.screen = screen
+        self.rect = pygame.Rect(3, 3, 6, 12)
+        self.color = 139, 195, 74
+        self.speed = 4
+        self.rect.centerx = gun.rect.centerx
+        self.rect.top = gun.rect.top
+        self.y = float(self.rect.y)
+
+
+    def update(self):
+        """перемещение пули вверх"""
+        self.y -= self.speed
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """рисуем пулю"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
